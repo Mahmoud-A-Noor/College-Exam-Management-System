5027016d49053f794c68a5825b0b0c214b115c3f
Mostafa Ashraf <mustashrf@gmail.com>
2023-08-08 17:40:58 +0300

    updated urls.py

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     path('<int:pk>/', UserRetrieveUpdateDeleteView.as_view(), name='user-detail'),
 
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token-refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
