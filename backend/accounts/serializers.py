944d61c5e6e3ab70a7f97224c2fbf02980d82967
mostafa <mustashrf@gmail.com>
2023-08-14 17:37:46 +0300

    added more read_only fields

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -8,9 +8,14 @@ class UserSerializer(serializers.ModelSerializer):
         extra_kwargs = {
             'date_of_birth': {'read_only': True},
             'gender': {'read_only': True},
+            'user_type': {'read_only': True},
             'date_joined': {'read_only': True},
-            'is_acttive': {'read_only': True},
+            'is_active': {'read_only': True},
+            'is_superuser': {'read_only': True},
             'is_staff': {'read_only': True},
+            'last_login': {'read_only': True},
+            'groups': {'read_only': True},
+            'user_permissions': {'read_only': True},
         }
 
 
