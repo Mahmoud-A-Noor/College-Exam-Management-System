2882dff6e892fca0847314b6aeae956e07f2ed86
mahmoudnoor <mahmoudnoor917@gmail.com>
2025-01-08 18:25:22 +0200

    Added production build

--- backend/courses/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Course, Enrollment, ExamAttempt, ExamQuestion, Exam
-
-admin.site.register(Course)
-admin.site.register(ExamAttempt)
-admin.site.register(ExamQuestion)
-admin.site.register(Exam)
-admin.site.register(Enrollment)
